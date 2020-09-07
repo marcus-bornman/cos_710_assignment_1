bdee93c8c476e808da07e99caf3f21af23290401
marcus-bornman <marcus.bornman@frei.one>
2020-09-07 13:21:02 +0200

    Considered all trees for fitness

--- src/main/java/problem/CovidProblem.java
+++ src/main/java/problem/CovidProblem.java
@@ -39,6 +39,16 @@ public class CovidProblem extends GPProblem implements SimpleProblemForm {
 	public final Map<LocalDate, Double> dateToConfirmedMap;
 
 	/**
+	 * A map with dates as keys and the number of confirmed cases as values.
+	 */
+	public final Map<LocalDate, Double> dateToDeathsMap;
+
+	/**
+	 * A map with dates as keys and the number of confirmed cases as values.
+	 */
+	public final Map<LocalDate, Double> dateToRecoveredMap;
+
+	/**
 	 * Reads the training data (COVID Observations) from training_data.csv to initialise the
 	 * problem instance accordingly.
 	 *
@@ -48,11 +58,17 @@ public class CovidProblem extends GPProblem implements SimpleProblemForm {
 		super();
 		observations = new DataReader().readTrainingData("training_data.csv");
 		dateToConfirmedMap = new HashMap<>();
+		dateToDeathsMap = new HashMap<>();
+		dateToRecoveredMap = new HashMap<>();
 		for (CovidObservation observation : observations) {
 			if (dateToConfirmedMap.containsKey(observation.date)) {
 				dateToConfirmedMap.put(observation.date, dateToConfirmedMap.get(observation.date) + observation.confirmed);
+				dateToDeathsMap.put(observation.date, dateToDeathsMap.get(observation.date) + observation.deaths);
+				dateToRecoveredMap.put(observation.date, dateToRecoveredMap.get(observation.date) + observation.recovered);
 			} else {
 				dateToConfirmedMap.put(observation.date, observation.confirmed);
+				dateToDeathsMap.put(observation.date, observation.deaths);
+				dateToRecoveredMap.put(observation.date, observation.recovered);
 			}
 		}
 	}
@@ -61,7 +77,6 @@ public class CovidProblem extends GPProblem implements SimpleProblemForm {
 	public void setup(final EvolutionState state, final Parameter base) {
 		super.setup(state, base);
 
-		// verify our input is the right class (or subclasses from it)
 		if (!(input instanceof PredictionData)) {
 			state.output.fatal("GPData class must subclass from " + PredictionData.class, base.push(P_DATA), null);
 		}
@@ -69,29 +84,38 @@ public class CovidProblem extends GPProblem implements SimpleProblemForm {
 
 	@Override
 	public void evaluate(final EvolutionState state, final Individual ind, final int subpopulation, final int threadnum) {
-		if (!ind.evaluated) {
-			PredictionData predictionData = (PredictionData) (this.input);
-			LocalDate minDate = observations.get(0).date.plus(Period.ofDays(windowSize + 1));
-			LocalDate maxDate = observations.get(observations.size() - 1).date.plus(Period.ofDays(1));
+		if (ind.evaluated) return;
 
-			long numDays = maxDate.toEpochDay() - minDate.toEpochDay();
-			int hits = 0;
-			double sum = 0.0;
-			for (int i = 0; i < numDays; i++) {
-				date = minDate.plus(Period.ofDays(i));
-				((GPIndividual) ind).trees[0].child.eval(state, threadnum, predictionData, stack, ((GPIndividual) ind), this);
-				double expected = dateToConfirmedMap.get(date);
-				double diff = Math.abs(expected - predictionData.num);
-				if ((diff / expected) < 0.05) hits++;
-				sum += (diff / expected);
-			}
+		PredictionData predictionData = (PredictionData) (this.input);
+		LocalDate minDate = observations.get(0).date.plus(Period.ofDays(windowSize + 1));
+		LocalDate maxDate = observations.get(observations.size() - 1).date.plus(Period.ofDays(1));
+		long numDays = maxDate.toEpochDay() - minDate.toEpochDay();
 
-			// the fitness better be KozaFitness!
-			KozaFitness fitness = ((KozaFitness) ind.fitness);
-			fitness.setStandardizedFitness(state, sum / numDays);
-			fitness.hits = hits;
-			ind.evaluated = true;
+		int hits = 0;
+		double sum = 0.0;
+		for (int i = 0; i < numDays; i++) {
+			date = minDate.plus(Period.ofDays(i));
+
+			((GPIndividual) ind).trees[0].child.eval(state, threadnum, predictionData, stack, ((GPIndividual) ind), this);
+			double actualConfirmed = dateToConfirmedMap.get(date);
+			double confirmedDiff = Math.abs(actualConfirmed - predictionData.num);
+
+			((GPIndividual) ind).trees[1].child.eval(state, threadnum, predictionData, stack, ((GPIndividual) ind), this);
+			double actualDeaths = dateToDeathsMap.get(date);
+			double deathsDiff = Math.abs(actualDeaths - predictionData.num);
+
+			((GPIndividual) ind).trees[2].child.eval(state, threadnum, predictionData, stack, ((GPIndividual) ind), this);
+			double actualRecovered = dateToRecoveredMap.get(date);
+			double recoveredDiff = Math.abs(actualRecovered - predictionData.num);
+
+			double singleFitness = ((confirmedDiff / actualConfirmed) + (deathsDiff / actualDeaths) + (recoveredDiff / actualRecovered)) / 3;
+			if (singleFitness < 0.05) hits++;
+			sum += singleFitness;
 		}
+
+		((KozaFitness) ind.fitness).setStandardizedFitness(state, sum / numDays);
+		((KozaFitness) ind.fitness).hits = hits;
+		ind.evaluated = true;
 	}
 }
 
